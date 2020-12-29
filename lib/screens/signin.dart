adfb0ab4ac0e280b068e826ec6bd04972eed1e20
basselallam <baseljahen@gmail.com>
2020-12-29 21:35:38 +0200

    commit

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -11,79 +11,78 @@ class _SignInState extends State<SignIn> {
 
 bool secure = true;
 
+TextEditingController emailController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
+      child: ListView(
+        scrollDirection: Axis.vertical,
         children: [
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.black, width: 1.0)
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.black, width: 1.0)
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.red, width: 1.0)
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.black, width: 1.0)
-              ),
-              prefixIcon: Icon(Icons.email, color: Colors.black, size: 20.0),
-              labelText: 'email address',
-              labelStyle: TextStyle(color: Colors.black, fontSize: 15.0)
-            ),
-            keyboardType: TextInputType.emailAddress,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.black, width: 1.0)
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.black, width: 1.0)
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.red, width: 1.0)
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.black, width: 1.0)
-              ),
-              prefixIcon: Icon(Icons.lock, color: Colors.black, size: 20.0),
-              labelText: 'password',
-              labelStyle: TextStyle(color: Colors.black, fontSize: 15.0),
-              suffixIcon: IconButton(
-                icon: Icon(Icons.remove_red_eye),
-                color: Colors.black,
-                iconSize: 20.0,
-                onPressed: () {
-                  setState(() {
-                    secure = !secure;
-                  });
-                }
-              )
-            ),
-            keyboardType: TextInputType.text,
-            obscureText: secure,
-          ),
+          field(Icons.email, 'email address', TextInputType.emailAddress, emailController),
+          field(Icons.lock, 'password', TextInputType.text, passwordController),
           Align(
             alignment: Alignment.bottomRight,
-            child: Text(
-              'Forgot Password?!',
-              style: TextStyle(color: Colors.grey, fontSize: 15.0, fontWeight: FontWeight.bold, height: 2.0),
+            child: InkWell(
+                onTap: () {},
+                child: Text(
+                'Forgot Password?!',
+                style: TextStyle(color: Colors.grey, fontSize: 15.0, fontWeight: FontWeight.bold, height: 2.0),
+              ),
             ),
           ),
-
+          FlatButton(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+            child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            color: Colors.black,
+            onPressed: () {}
+          ),
         ],
+      ),
+    );
+  }
+  field(IconData icon, String label, TextInputType type, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.black, width: 1.0)
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.black, width: 1.0)
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.red, width: 1.0)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.black, width: 1.0)
+          ),
+          prefixIcon: Icon(icon, color: Colors.black, size: 20.0),
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.black, fontSize: 15.0),
+          suffixIcon: label == 'password' ? IconButton(
+            icon: Icon(Icons.remove_red_eye),
+            color: Colors.black,
+            iconSize: 20.0,
+            onPressed: () {
+              setState(() {
+                secure = !secure;
+              });
+            }
+          ) : null
+        ),
+        keyboardType: type,
+        obscureText: label == 'password' ? secure : false,
+        controller: controller
       ),
     );
   }
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,3 +1,4 @@
+import 'package:anime/screens/bottomnavbar/bottomnavbar.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,10 +10,212 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+
+bool secure = true;
+
+TextEditingController emailController = TextEditingController();
+TextEditingController userNameController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
+TextEditingController confirmPasswordController = TextEditingController();
+
+GlobalKey<FormState> emailKey = GlobalKey<FormState>();
+GlobalKey<FormState> userNameKey = GlobalKey<FormState>();
+GlobalKey<FormState> passwordKey = GlobalKey<FormState>();
+GlobalKey<FormState> confirmPasswordKey = GlobalKey<FormState>();
+
+final _formKey = GlobalKey<FormState>();
+
+String gender = 'Select Gender';
+
+bool check = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Form(
+          key: _formKey,
+          child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            field(Icons.account_box, 'user name', TextInputType.text, userNameController, false, null, userNameKey),
+            field(Icons.email, 'email address', TextInputType.emailAddress, emailController, false, null, emailKey),
+            field(Icons.lock, 'password', TextInputType.text, passwordController, secure, lockButton(), passwordKey),
+            field(Icons.lock, 'confirm password', TextInputType.text, confirmPasswordController, secure, lockButton(), confirmPasswordKey),
+            item('Gender', gender,
+            PopupMenuButton(
+                icon: Icon(Icons.arrow_circle_down, color: Colors.grey, size: 20.0),
+                itemBuilder: (BuildContext context){
+                  return <PopupMenuEntry<String>>[
+                    PopupMenuItem(
+                      child: Text(
+                        'Male',
+                        style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      value: 'Male'
+                    ),
+                    PopupMenuItem(
+                      child: Text(
+                        'Female',
+                        style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      value: 'Female'
+                    ),
+                  ];
+                },
+                onSelected: (value){
+                  setState((){
+                    gender = value;
+                  });
+                },
+              ),
+              () {}
+            ),
+            item('Terms & Condtions', 'Read our Terms & Conditions',
+            Checkbox(
+              activeColor: Colors.black,
+              checkColor: Colors.white,
+              hoverColor: Colors.black,
+              value: check,
+              onChanged: (value) {
+                setState((){
+                  check = value;
+                });
+              },
+            ),
+            () {
+              showModalBottomSheet(
+                context: context,
+                backgroundColor: Colors.white,
+                builder: (BuildContext context){
+                  return ListTile(
+                    title: Text(
+                        'Our Terms and Conditions',
+                        style: TextStyle(color: Colors.black, fontSize: 25.0, fontWeight: FontWeight.bold),
+                      ),
+                    subtitle: Text(
+                      '1- your data is unsaffe\n2- dont use our app\n3- abo el magad whos developed it\n1- your data is unsaffe\n2- dont use our app\n3- abo el magad whos developed it',
+                      style: TextStyle(color: Colors.grey, fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  );
+                }
+              );
+            }
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+              child: Text(
+                  'Signup',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              color: Colors.black,
+              onPressed: () {
+                if(!_formKey.currentState.validate()){
+                  return Scaffold.of(context).showSnackBar(
+                    snack('some fields required!'),
+                  );
+                }else if(passwordController.text != confirmPasswordController.text){
+                  return Scaffold.of(context).showSnackBar(
+                    snack('password dont matched'),
+                  );
+                }else if(gender == 'Select Gender'){
+                  return Scaffold.of(context).showSnackBar(
+                    snack('select your gender'),
+                  );
+                }else if(check == false){
+                  return Scaffold.of(context).showSnackBar(
+                    snack('kindly accept terms & conditions'),
+                  );
+                }else{
+                  return Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) {return BottomNavBar();}));
+                }
+              }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  snack(String content) {
+    return SnackBar(
+      content: Text(
+        content,
+        style: TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Colors.red,
+      duration: Duration(seconds: 4),
+    );
+  }
+  item(String title, String subTitle, Widget trailing, Function onTap) {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        border: Border.all(color: Colors.black, width: 1.0),
+      ),
+      child: ListTile(
+        title: Text(
+            title,
+            style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+        subtitle: Text(
+            subTitle,
+            style: TextStyle(color: Colors.grey, fontSize: 20.0, fontWeight: FontWeight.normal),
+          ),
+        trailing: trailing,
+        onTap: onTap
+      ),
+    );
+  }
+  lockButton() {
+    return IconButton(
+      icon: Icon(Icons.remove_red_eye),
+      color: Colors.black,
+      iconSize: 20.0,
+      onPressed: () {
+        setState(() {
+          secure = !secure;
+        });
+      }
+    );
+  }
+  field(IconData icon, String label, TextInputType type, TextEditingController controller, bool obsecure, Widget lock, Key key) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        key: key,
+        validator: (value) {
+          if(value.isEmpty){
+            return 'this field required';
+          }else{
+            return null;
+          }
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.black, width: 1.0)
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.black, width: 1.0)
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.red, width: 1.0)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: BorderSide(color: Colors.black, width: 1.0)
+          ),
+          prefixIcon: Icon(icon, color: Colors.black, size: 20.0),
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.black, fontSize: 15.0),
+          suffixIcon: lock
+        ),
+        keyboardType: type,
+        obscureText: obsecure,
+        controller: controller
+      ),
     );
   }
 }
